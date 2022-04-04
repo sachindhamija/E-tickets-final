3d028910897703476924ba691f5f6f4af630721a
D C <D C@DESKTOP-S0JJRN6>
2022-04-04 11:20:04 +0530

    Add project files.

---
+++ E-tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
